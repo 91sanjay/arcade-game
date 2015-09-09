2b69033c12292157b4683e0355a97d56549faf11
Sanjay Rajashekhar <91sanjay@gmail.com>
2015-09-08 23:20:16 -0400

    Add player class and movement
    1. Add player class with required methoods
    2. Add player movements according to key strokes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
+var allEnemies = [];
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = x;
+    this.y = y;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -14,6 +18,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,11 +30,41 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-boy.png'
+}
 
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y)
+}
+
+Player.prototype.update = function(dt) {
+
+}
+
+Player.prototype.handleInput = function(key) {
+    if (key == "right" && this.x < 404) {
+        this.x += 101;
+    } else if (key == "left" && this.x > 0) {
+        this.x -= 101;
+    } else if (key == "up" && this.y > 101) {
+        this.y -= 83;
+    } else if (key == "down" && this.y < 398) {
+        this.y += 83;
+    }
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var player = new Player(0, 398);
+var enemy1 = new Enemy(-98,52);
+var enemy2 = new Enemy(-98,152);
+
+allEnemies.push(enemy1);
+allEnemies.push(enemy2);
 
 
 // This listens for key presses and sends the keys to your
