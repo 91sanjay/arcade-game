58622bbaa220015fc9625ac187764f9acea5a531
Sanjay Rajashekhar <91sanjay@gmail.com>
2015-09-09 21:45:15 -0400

    Add enemy moving capability and player reset on collision/win
    1. Add enemies that constantly move at different speeds
    2. Add player functionality to detect win/loss

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
+var ROW1 = "Row1";
+var ROW2 = "Row2";
+var ROW3 = "Row3";
+var score = 0;
 var allEnemies = [];
+var totalEnemies = 4;
+var score = 0;
+var enemyPositions = {};
+
+enemyPositions.ROW1 = [];
+enemyPositions.ROW2 = [];
+enemyPositions.ROW3 = [];
+
+var getRandomPosition = function() {
+    var positionArray = [60, 140, 230];
+    var i = Math.floor(Math.random()*(positionArray.length));
+    // console.log(positionArray[i]);
+    return positionArray[i];
+}
 
 // Enemies our player must avoid
 var Enemy = function(x, y) {
@@ -6,6 +24,7 @@ var Enemy = function(x, y) {
     // we've provided one for you to get started
     this.x = x;
     this.y = y;
+    this.speed = Math.floor((Math.random() * 250) + 125);
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -18,7 +37,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-
+        if (this.x < 505) {
+            this.x = this.x + this.speed * dt;
+        } else {
+            this.x = -98;
+        }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -41,7 +64,9 @@ Player.prototype.render = function() {
 }
 
 Player.prototype.update = function(dt) {
-
+    if (checkCollision(this)) {
+        this.y = 398*dt;
+    }
 }
 
 Player.prototype.handleInput = function(key) {
@@ -49,23 +74,75 @@ Player.prototype.handleInput = function(key) {
         this.x += 101;
     } else if (key == "left" && this.x > 0) {
         this.x -= 101;
-    } else if (key == "up" && this.y > 101) {
-        this.y -= 83;
+    } else if (key == "up") {
+        if(this.y > 101) {
+            this.y -= 83;
+        } else {
+            this.y = 398;
+        }
+        // console.log("y "+this.y);
     } else if (key == "down" && this.y < 398) {
         this.y += 83;
+    }
+}
+
+var checkCollision = function(player) {
+    if (player.y < 100) {
+        var enemies = enemyPositions.ROW1;
+        enemies.forEach(function(enemy){
+            if (compareDistance(player,enemy)) {
+                return true;
+            }
+        });
+    } else if (player.y < 200) {
+        var enemies = enemyPositions.ROW2;
+        enemies.forEach(function(enemy){
+            if (compareDistance(player,enemy)) {
+                return true;
+            }
+        });
+    } else {
+        var enemies = enemyPositions.ROW3;
+        enemies.forEach(function(enemy){
+            if (compareDistance(player,enemy)) {
+                return true;
+            }
+        });
+    }
+}
+
+var compareDistance = function(player, enemy) {
+    if (Math.abs(enemy.x - player.x) < 50 && player.y<300) {
+        console.log("COLLISION");
+        player.y = 398;
+        return true;
     }
 }
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var pos = getRandomPosition();
 var player = new Player(0, 398);
-var enemy1 = new Enemy(-98,52);
-var enemy2 = new Enemy(-98,152);
+var enemy1 = new Enemy(98, 60);
+var enemy2 = new Enemy(98, 140);
+var enemy3 = new Enemy(98, 230);
+var enemy4 = new Enemy(-98, pos);
 
 allEnemies.push(enemy1);
 allEnemies.push(enemy2);
+allEnemies.push(enemy3);
+allEnemies.push(enemy4);
 
+allEnemies.forEach(function(enemy) {
+    console.log(enemy.y);
+    if (enemy.y < 100) {
+        enemyPositions.ROW1.push(enemy);
+    } else if (enemy.y > 100 && enemy.y < 200) {
+        enemyPositions.ROW2.push(enemy);
+    } else {
+        enemyPositions.ROW3.push(enemy);
+    }
+});
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
